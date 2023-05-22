fcde501671192a125e639b7405865a3813405330
Gurveer Madurai <maduraigurveer@gmail.com>
2023-05-21 22:37:47 -0700

    post logic added to notes.js

--- routes/notes.js
+++ routes/notes.js
@@ -1,25 +1,39 @@
 const notes = require("express").Router();
 // const writeFile = require('fs');
-const {uuidv4} = require('uuidv4');
+const { v4: uuidv4 } = require("uuid");
+const { writeFile, readFile, appendFile } = require("fs");
 let data = require("../db/db.json");
 
-// const { v4: uuidv4 } = require('uuid');
-
-const uuid = (newID) => {
-    newID.id = uuidv4();
-    return newID;
-}
+const appendToFile = (appendToFile) => {
+  appendFile("./db/db.json", JSON.stringify(appendToFile), (err) => {
+    if (err) console.log(err);
+  });
+};
 
 notes.get("/", (req, res) => {
   res.json(data);
   console.log("GET request recieved");
 });
 
-notes.post('/', (req,res)=> {
-    data.push(uuid(req.body));
+notes.post("/", (req, res) => {
+  const { title, text } = req.body;
+  if (req.body) {
+    const newNote = [
+      {
+        title,
+        text,
+        id: uuidv4(),
+      },
+    ];
+    appendToFile(newNote, "./db/db.json");
+    res.json(`Note added successfully 🚀`);
+  } else {
+    res.error("Error in adding tip");
+  }
+
   // Log the response body to the console
   console.log(req.body);
-    console.log('POST req recieved');
-})
+  console.log("POST req recieved");
+});
 
 module.exports = notes;
