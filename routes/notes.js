39fe7076ba39ca5d68c1fb1a82632bf8f244d499
Gurveer Madurai <maduraigurveer@gmail.com>
2023-05-22 00:17:42 -0700

    note.post data pulled and written into json file

--- routes/notes.js
+++ routes/notes.js
@@ -1,39 +1,29 @@
 const notes = require("express").Router();
 // const writeFile = require('fs');
+const fs = require("fs");
 const { v4: uuidv4 } = require("uuid");
-const { writeFile, readFile, appendFile } = require("fs");
-let data = require("../db/db.json");
+// const { writeFile, readFile, appendFile } = require("fs");
+// let data = require("../db/db.json");
 
-const appendToFile = (appendToFile) => {
-  appendFile("./db/db.json", JSON.stringify(appendToFile), (err) => {
-    if (err) console.log(err);
-  });
-};
+const data = fs.readFileSync("./db/db.json", { encoding: "utf8" });
+// Display the file data
+console.log(data);
+
+// parse stringified data into an object
+const parsedData = JSON.parse(data);
+
+notes.post("/", (req, res) => {
+  parsedData.push({ id: uuidv4(), ...req.body });
+  const updatedData = JSON.stringify(parsedData);
+
+  //saves updated data to db.json file
+  fs.writeFileSync("./db/db.json", updatedData, { encoding: "utf8" });
+  res.json(`Note added successfully 🚀`);
+});
 
 notes.get("/", (req, res) => {
   res.json(data);
   console.log("GET request recieved");
-});
-
-notes.post("/", (req, res) => {
-  const { title, text } = req.body;
-  if (req.body) {
-    const newNote = [
-      {
-        title,
-        text,
-        id: uuidv4(),
-      },
-    ];
-    appendToFile(newNote, "./db/db.json");
-    res.json(`Note added successfully 🚀`);
-  } else {
-    res.error("Error in adding tip");
-  }
-
-  // Log the response body to the console
-  console.log(req.body);
-  console.log("POST req recieved");
 });
 
 module.exports = notes;
